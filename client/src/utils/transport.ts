07a13d4fef3f13dcad50cbee60d0d46cc97ed6e8
DahakaLab <dahakalab@gmail.com>
2023-08-31 12:55:25 +0300

    .env has been added, finilized readme

--- client/src/utils/transport.ts
+++ client/src/utils/transport.ts
@@ -1,7 +1,13 @@
 import axios from 'axios';
 
+const defaultApiUrl = 'http://127.0.0.1:3000';
+const envApiUrl: string | undefined = import.meta.env.VITE_API_URL;
+const apiUrl = envApiUrl || defaultApiUrl;
+
+console.log('envApiUrl', envApiUrl);
+
 export const transport = axios.create({
-	baseURL: 'http://127.0.0.1:3000',
+	baseURL: apiUrl,
 	headers: {
 		'Content-Type': 'application/json'
 	}
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,12 +1,24 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import svg from '@poppanator/sveltekit-svg';
 
-export default defineConfig({
-	plugins: [
-		sveltekit(),
-		svg({
-			type: 'component'
-		})
-	]
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, process.cwd(), '')
+	const devPort = env.DEV_PORT ? parseInt(env.DEV_PORT) ?? 5173 : 5173;
+	const prevPort = env.PREV_PORT ? parseInt(env.PREV_PORT) ?? 4173 : 4173;
+
+	return {
+		preview: {
+			port: prevPort,
+		},
+		server: {
+			port: devPort,
+		},
+		plugins: [
+			sveltekit(),
+			svg({
+				type: 'component',
+			})
+		]
+	};
 });
