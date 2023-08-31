35c31755084e0dfa807adf33c7203ccb761873c5
DahakaLab <dahakalab@gmail.com>
2023-08-31 12:59:41 +0300

    Fix github actions

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,21 +3,21 @@ import { defineConfig, loadEnv } from 'vite';
 import svg from '@poppanator/sveltekit-svg';
 
 export default defineConfig(({ mode }) => {
-	const env = loadEnv(mode, process.cwd(), '')
+	const env = loadEnv(mode, process.cwd(), '');
 	const devPort = env.DEV_PORT ? parseInt(env.DEV_PORT) ?? 5173 : 5173;
 	const prevPort = env.PREV_PORT ? parseInt(env.PREV_PORT) ?? 4173 : 4173;
 
 	return {
 		preview: {
-			port: prevPort,
+			port: prevPort
 		},
 		server: {
-			port: devPort,
+			port: devPort
 		},
 		plugins: [
 			sveltekit(),
 			svg({
-				type: 'component',
+				type: 'component'
 			})
 		]
 	};
